fee5c44a0caeb8d827849950eb7fec48becb97b3
joshua oguntimehin <joshuaoguntimehin46@gmail.com>
2024-03-13 08:47:03 -0700

    new

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def f_docTohex(n):
-   return hex(n)
-
 if __name__ == '__main__':
-   for number in range(0, 99):
-    if number < 98:
-        print(number, end=", ")
-    else:
-        print(number, end="")
+    for i in range(0, 10):
+        for j in range(0, 10):
+            if i == 9 and j == 9:
+                print("{}{}".format(i, j),end="")
+            else:
+                print("{}{}".format(i, j),end=", ")
