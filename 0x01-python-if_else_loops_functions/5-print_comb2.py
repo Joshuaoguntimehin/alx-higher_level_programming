11d1b438a5da71d347bcf255b0fb9aed00bdc9ec
joshua oguntimehin <joshuaoguntimehin46@gmail.com>
2024-03-13 08:51:27 -0700

    new

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     for i in range(0, 10):
         for j in range(0, 10):
             if i == 9 and j == 9:
-                print("{}{}".format(i, j),end="")
+                print("{}{}".format(i, j), end="")
             else:
-                print("{}{}".format(i, j),end=", ")
+                print("{}{}".format(i, j), end=", ")
