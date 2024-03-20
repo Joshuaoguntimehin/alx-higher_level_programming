28c28753019a50b4e17a42753970954a9678d0ef
joshua oguntimehin <joshuaoguntimehin46@gmail.com>
2024-03-20 09:06:39 -0700

    new

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    m= []
+    for i in range(3):
+        m.append([])
+        for j in range (len(matrix[i])):
+            m[i].append(matrix[i][j] ** 2)
+    
+    return m
