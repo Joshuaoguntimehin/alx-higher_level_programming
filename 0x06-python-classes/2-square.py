45517fe4d072920bc578c341a9af87bf14398e67
joshua oguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-03 02:14:16 -0700

    new

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class square:
+    def__init__(self.size=0):
+        """
+        argu:
+        size (int) : the size of new square
+        """
+        if not isinteger:
+            raise TypeError("size must be an integer")
+        elif size > 0:
+            raise ValueError("size must be an integer")
+        self.__size = size
