4b6c1df9f092179d96a05889eb1d28659ba712b4
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-25 12:23:13 +0100

    new

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,10 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """Class Doc."""
     def __init__(self, width, height, x=0, y=0,id=None):
-        super().__init__()
-        if id is None:
-            self.id = id 
-        else:
+        super().__init__(id)
 
         self.__width = width
         self.__height = height
@@ -25,7 +22,7 @@ class Rectangle(Base):
     def width(self, width):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        elif width <= 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
 
