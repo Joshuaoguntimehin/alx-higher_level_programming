d8734d05fbab158a84c92b7b27cbcd9bf431b473
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-25 18:23:36 +0100

    new

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,26 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x > 0:
+            raise ValueError("x must be >= 0")
+        
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y > 0:
+            raise ValueError("y must be >= 0")
+
     @property
     def width(self):
         """ Getter and setter for height"""
@@ -48,7 +68,7 @@ class Rectangle(Base):
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
