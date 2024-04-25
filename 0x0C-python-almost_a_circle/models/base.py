cbd4414eb24f68c787980b4632e4963df20b1721
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-25 12:01:39 +0100

    new

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 class Base:
+    '''class doc'''
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """using if statement for id"""
         if id is not None:
             self.id = id
         else:
+            """for if id id none"""
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
