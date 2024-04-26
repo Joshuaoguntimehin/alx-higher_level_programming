fae12c980fd121b8de2f5bfd831014cf537bc5b3
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-26 12:38:32 +0100

    new

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""
+Defines a square class.
+"""
 from models.rectangle import Rectangle
-"""import statement"""
 
 
 class Square(Rectangle):
-    """documetation"""
+    """
+    Represent a square.
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = siz
+        """
+        Initialize a new Square.
+        """
+        self.size = size
         super().__init__(x=x, y=y, width=size, height=size, id=id)
 
     def __str__(self):
