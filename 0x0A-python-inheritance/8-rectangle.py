fdedd7a1f9626266309fb211d93eb3b855c32e7c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-27 13:08:26 +0100

    new

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+
+
 class BaseGeometry:
     pass
 class Rectangle(BaseGeometry):
+    '''A subclass representing a rectangle.'''
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
