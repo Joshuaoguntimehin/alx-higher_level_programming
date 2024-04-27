c69a7d42e2f078c410c8b8f9197dbcb45793b0e0
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-27 13:53:13 +0100

    new

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,17 +4,16 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''A subclass representing a rectangle.'''
-    def __init__(self, size):
-        '''Constructor.'''
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+     '''A subclass representing a rectangle.'''
+     def __init__(self, size):
+        self._size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
 
-    def area(self):
+     def area(self):
         '''Method for area of square.'''
-        return self.__size ** 2
+        return self._size ** 2
 
-    def __str__(self):
-        '''Returns string representation of this square.'''
-        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+     def __str__(self):
+        return f"[Square] {self._size}/{self._size}"
+
