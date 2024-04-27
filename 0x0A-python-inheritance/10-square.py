b98581bd36042b485c3a92f1ce13eebce1bb6aa6
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-04-27 13:42:41 +0100

    new

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,22 @@
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+
+
 class Rectangle:
     pass
 
+
 class Square(Rectangle):
+    '''A subclass representing a rectangle.'''
     def __init__(self, size):
         self._size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
-        def area(self):
-            return self.size ** 2
-        def __str__(self):
-            return f"[Square] {self._size}/{self._size}"
+
+    def area(self):
+        '''Method for area of square.'''
+        return self._size ** 2
+
+    def __str__(self):
+        return f"[Square] {self._size}/{self._size}"
 
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,17 +1,6 @@
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
 def inherits_from(obj, a_class):
-    """
-    Check if the object 'obj' is an instance of a class that inherited (either directly or
-    indirectly) from 'parent_class'.
-
-    Parameters:
-        obj (object): The object to check.
-        parent_class (type): The parent class to check against.
-
-    Returns:
-        bool: True if 'obj' is an instance of a class that inherited from 'parent_class',
-              otherwise False.
-    """
-    if  isinstance(obj, a_class) and issubclass(type(obj), a_class):
-        return True
-    else:
-        return False
+    '''Determines if an object is a true subclass of a class.'''
+    return isinstance(obj, a_class) and type(obj) != a_class
+        
