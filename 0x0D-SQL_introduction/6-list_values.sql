f2f441b70c3876266bf8ceb742ab7c48a4abe294
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-05-23 16:39:20 +0100

    alx tasks

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,5 +1,3 @@
 -- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-SELECT TABLE_NAME
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0';
+SELECT * FROM first_table;
