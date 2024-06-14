77bc831edf4ea6e00e825b4dd9eb07deb965603c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-14 13:22:00 +0100

    alx tasks

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,6 @@
 def safe_print_integer(value):
     try:
-                if not isinstance(value, int):
-                    return False
-                print("{:d}".format(value))
-    except IndexError:
-        pass
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
