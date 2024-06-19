065a3534febff445f29fb4abf32abe9f3ee206b2
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-19 18:39:11 +0100

    alx tasks

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const times = parseInt(process.argv[2]);
+const message = "C is fun";
+
+if(!isNaN(times) && times > 0){
+	for(let i =0; i < times; i++){
+		console.log(message);
+	}
+}else{
+	console.log("Missing number of occurrences")
+}
