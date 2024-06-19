aeddf428aaa2b2aaabac66d5fb41b19b92192559
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-19 16:11:14 +0100

    alx tasks

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if(args[0]=== undefined){
+	console.log('No argument')
+}else if (args[1] === undefined){
+	console.log( args[0]);
+}else{
+	args.forEach((arg, index) => {
+		console.log(`${index + 1}: ${arg}`);
+	});
+}
