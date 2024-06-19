ec4601f177b33f008b9367cd4613d0d2af71bdaa
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-19 18:25:20 +0100

    alx tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const message = ' C is fun\n Python is cool \n JavaScript is amazing'
-console.log(message)
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < messages.length; i++){
+	console.log(messages[i]);
+}
