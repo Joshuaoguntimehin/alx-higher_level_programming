6f3368864639ad246eef5b7e9b20eba78186578b
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-21 00:03:19 +0100

    alx tasks

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      class Rectangle {//enpty}
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // If w or h is 0 or not a positive integer, create an empty object
+            return Object.create(null);
+        }
+
+        // Initialize the attributes only if w and h are positive integers
+        this.width = w;
+        this.height = h;
     }
-    this.width = w;
-    this.height = h;
-  }
-	  print() {
-		  if (!this.width || !this.height) {
-			  console.log('rectangle is empty');
-			  return;
-		  }
-		  for (let i = 0; i < this.height; i++) {
-			  let row = '';
-		  }
-		  for (let j = 0; j < this.width; j++) {
-			  row += 'x';
-		  }
-		  console.log(row);
-	  }
-  }
+
+    // Instance method to print the rectangle using the character 'X'
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('x'.repeat(this.width));
+	}
+    }
 }
 module.exports = Rectangle;
