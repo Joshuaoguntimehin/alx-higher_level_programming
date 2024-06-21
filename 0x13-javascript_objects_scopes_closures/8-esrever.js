58ce345482b32e8ea342ec960baf66b3b6991536
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-21 16:38:58 +0100

    alx tasks

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,16 +3,18 @@ exports.esrever = function (list) {
   if (list.length === 0) {
     return [];
   }
+
   let start = 0;
   let end = list.length - 1;
 
   while (start < end) {
     const temp = list[start];
     list[start] = list[end];
-    list[end = temp];
+    list[end] = temp;
 
     start++;
     end--;
   }
+
   return list;
 };
