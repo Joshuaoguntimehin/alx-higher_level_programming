9d7c3fddbf40874a1de84cb82f37d5f8c0b17652
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-21 14:47:46 +0100

    alx tasks

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4.rectangle');
+const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle{
-    constructor (size){
-        super(size, size);
+class Square extends Rectangle {
+  constructor (size) {
+    if (Number.isInteger(size) && size > 0) {
+      super(size, size);
+    } else {
+      super(undefined, undefined); // This creates an empty object if size is invalid
     }
+  }
 }
 
 module.exports = Square;
