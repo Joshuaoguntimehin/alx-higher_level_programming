5c1de58692caaf20966c00ffdc32408e798ce233
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-24 15:02:56 +0100

    alx tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,13 +4,16 @@ import MySQLdb
 import sys
 """placing value"""
 
-db = MySQLdb.connect(host = "localhost", port = 3306 , user=sys.argv[1], 
-                   passwd=sys.argv[2], db =sys.argv[3] )
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
-cur.execute("SELECT name FROM states WHERE name LIKE 'N%';")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host = "localhost", port = 3306 , user=sys.argv[1], 
+                         passwd=sys.argv[2], db =sys.argv[3] )
+    cur = db.cursor()
+    cur.execute("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY name ASC;")
+    count = 1
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(f"{count}.{row}")
+        count += 1
+    cur.close()
+    db.close()
