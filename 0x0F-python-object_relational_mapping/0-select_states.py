e5e17833371a7a32ca66d658c7d2274bbfc5af3b
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-24 13:14:33 +0100

    alx tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,12 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database_name):
     # Connect to MySQL
     try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database_name)
+        db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                             passwd=password, db=database_name)
         cursor = db.cursor()
 
         # Execute the query
@@ -25,16 +27,14 @@ def list_states(username, password, database_name):
         print(f"Error connecting to MySQL: {e}")
         sys.exit(1)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <username> <password> <database_name>")
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
     list_states(username, password, database_name)
-
-
-
