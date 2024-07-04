3b10d1cb1ff06e11d8770be333ad564be134e79c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-05 00:19:28 +0100

    alx tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,6 +3,6 @@ if [ -z "$1" ]; then
 	echo "USEGE : $0 <URL>"
 	EXIT 1
 fi
-URL=$1
-size=$(curl -s -o /dev/null -w '%{size_download}'"$URL")
+URL="http://$URL"
+size=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
 echo "$size"
