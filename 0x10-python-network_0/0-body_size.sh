889a61d37b699dede96e0c2777cb3ac63b9d70db
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-05 00:23:34 +0100

    alx tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
 if [ -z "$1" ]; then
-	echo "USEGE : $0 <URL>"
-	EXIT 1
+    echo "USAGE: $0 <URL>"
+    exit 1
 fi
-URL="http://$URL"
+
+URL=$1
+
+# Check if the URL starts with "http://" or "https://"
+if [[ ! "$URL" =~ ^https?:// ]]; then
+    URL="http://$URL"
+fi
+
+# Use curl to get the size of the response body in bytes
 size=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
-echo "$size"
+echo "Size: $size bytes"
+
