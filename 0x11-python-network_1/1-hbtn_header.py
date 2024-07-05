5f676a05a2c95409f8e38fd8c71b06695a5d1fd3
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-05 16:54:15 +0100

    alx tasks

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import urllib.request
+import sys
+
+
+url = "https://alx-intranet.hbtn.io"
+with urllib.request.urlopen(url) as response:
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print(f"{x_request_id}")
+    else:
+        print("X-Request-Id header not found in the response.")
