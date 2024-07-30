2f50222c4ae154f551766778f3ea9918d081fc04
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-30 18:17:06 +0100

    alx tasks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// Import the file system module
+""" Import the file system module"""
 const fs = require('fs');
 
-// Check if the correct number of arguments is provided
+""" Check if the correct number of arguments is provided"""
 if (process.argv.length < 4) {
   console.error('Usage: node writeFile.js <file-path> <data-to-write>');
   process.exit(1);
@@ -14,11 +14,11 @@ const data = process.argv[3];
 
 // Write the data to the file asynchronously
 fs.writeFile(filePath, data, 'utf8', (err) => {
-  if (err) {
-    // Handle the error if there is one
-    console.error('Error writing to the file:', err.message);
-    return;
-  }
-  // Confirm that the file was written successfully
-  console.log(data);
+	if (err) {
+		// Handle the error if there is one
+		console.error('Error writing to the file:', err.message);
+		return;
+	}
+	// Confirm that the file was written successfully
+	console.log(data);
 });
